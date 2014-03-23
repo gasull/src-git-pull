fe4e132432dafda2dbeaa32762041d9fbd1dbe8d
Daniel Gonzalez Gasull <gasull@gmail.com>
2014-03-23 14:32:58 -0700

    Check that settings exists

--- fabfile.py
+++ fabfile.py
@@ -4,10 +4,16 @@
 #
 # Author: Daniel Gonzalez Gasull
 
+import sys
+
 from fabric import api as fab_api
 from fabric import exceptions as fab_ex
 
-import settings
+try:
+    import settings
+except ImportError:
+    print('No settings file')
+    sys.exit()
 
 
 fab_api.env.use_ssh_config = True
